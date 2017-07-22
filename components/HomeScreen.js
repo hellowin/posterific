bb4df94fe0122d15edbb64579bd496635f012f1a
Andi Nugroho Dirgantara <andi.n.dirgantara@gmail.com>
2017-07-22 10:57:44 +0700

    error on account kit deps

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { TouchableOpacity, View, Image, Text, StyleSheet } from 'react-native';
 import storage from '../Model/PosterificStorage';
-import { LoginButton } from 'react-native-fbsdk';
+import { LoginButton, AccessToken } from 'react-native-fbsdk';
 
 export default class HomeScreen extends React.Component {
   constructor(props) {
@@ -25,7 +25,15 @@ export default class HomeScreen extends React.Component {
             } else if (res.isCancelled) {
               alert('Login was cancelled.');
             } else {
-              alert('Login was ok.');
+              AccessToken.getCurrentAccessToken().then(data => {
+                if (data === null) {
+                  console.warn('no token');
+                } else {
+                  console.log(data.accessToken);
+                  console.log(data.permissions);
+                }
+              });
+              this.props.navigator.push({ name: 'PosterList' });
             }
           }}
         />
